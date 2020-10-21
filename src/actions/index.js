59b97f0e2b5549f82facffe2760f8e66292d37ea
Sergiyy <sergiy.mohurenko@gmail.com>
2020-10-21 14:30:45 +0300

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = '268aaf68afa1dd5e75433ad8f4b55e7b';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  console.log('Request: ', request);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
